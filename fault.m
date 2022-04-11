764eb54815853a41e7b1c61e5a988df36223d8c9
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-04-11 09:49:34 +0800

    add matlab files

---
+++ fault.m
@@ -0,0 +1,17 @@
+clear all;
+clc;
+
+DSSObj = actxserver('OpenDSSEngine.DSS');
+% Start the DSS. Only needs to be executed the first time w/in a
+% Matlab session
+Start = DSSObj.Start(0);
+
+DSSText = DSSObj.Text;
+DSSCircuit=DSSObj.ActiveCircuit;
+DSSSolution=DSSCircuit.Solution;
+DSSText.command='Compile (C:\Users\Afiq Hafizuddin\Documents\MATLAB\01-STATIC-DUMA-AMEND\fault-master.dss)';
+
+DSSText.command = 'Solve';
+DSSText.command ='solve mode=faultstudy';
+DSSText.command ='show Faults';
+DSSText.command ='export Faultstudy';
---
+++ main.m
@@ -0,0 +1,27 @@
+clear all;
+clc;
+
+DSSObj = actxserver('OpenDSSEngine.DSS');
+Start=DSSObj.Start(0);
+
+DSSText = DSSObj.Text;
+DSSCircuit = DSSObj.ActiveCircuit;
+DSSSolution = DSSCircuit.Solution;
+DSSText.command = 'Compile (C:\Users\Afiq Hafizuddin\Documents\MATLAB\01-STATIC-DUMA-AMEND\master.dss)';
+
+% Voltage Profile
+% DSSText.command = 'Show voltage elem';
+% DSSText.command = 'Export voltage';
+% DSSText.command = 'Export seqvoltages';
+DSSText.command = 'Show Losses';
+% DSSText.command = 'Show current elem';
+% DSSText.command = 'Export seqcurrents';
+
+
+
+% The other things
+% DSSText.command = 'Show power kVA elem';
+% DSSText.command = 'Export power kVA';
+% DSSText.command = 'Export current';
+
+
